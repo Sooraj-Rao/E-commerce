1b78f6455112792f207f212336886c34dc8520a4
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-01-11 12:53:18 +0530

    Minimal Changes Done!

--- Client/public/Constants/Data.jsx
+++ Client/public/Constants/Data.jsx
@@ -1,21 +1,21 @@
 export const CategoryData = [
     {
-        "Men's Fashion": '../../../../public/Images/Home/Category/men.webp'
+        "Men's Fashion": '../Images/Home/Category/men.webp'
     },
     {
-        "Women's Fashion": '../../../../public/Images/Home/Category/women.webp'
+        "Women's Fashion": '../Images/Home/Category/women.webp'
     },
     {
-        Books: '../../../../public/Images/Home/Category/Books.webp'
+        Books: '../Images/Home/Category/books.webp'
     },
     {
-        Electronics: '../../../../public/Images/Home/Category/electronics.jpg'
+        Electronics: '../Images/Home/Category/electronics.jpg'
     },
     {
-        Home: '../../../../public/Images/Home/Category/home.webp'
+        Home: '../Images/Home/Category/home.webp'
     },
     {
-        Smartphone: '../../../../public/Images/Home/Category/mobile.webp'
+        Smartphone: '../Images/Home/Category/mobile.webp'
     },
     {
         Audio: '../Images/Home/Category/audio.webp'
--- Client/src/Main/Components/Home/Hero.jsx
+++ Client/src/Main/Components/Home/Hero.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from 'react'
-import Banner1 from '../../../../public/Images/Home/banner2.jpg'
-import Banner2 from '../../../../public/Images/Home/banner3.jpg'
-import Banner5 from '../../../../public/Images/Home/banner4.jpg'
-import Banner6 from '../../../../public/Images/Home/banner5.png'
-import Banner7 from '../../../../public/Images/Home/banner6.png'
-import Banner8 from '../../../../public/Images/Home/sale-banner.png'
+import Banners1 from '../../../../public/Images/Home/banners1.jpg'
+import Banners2 from '../../../../public/Images/Home/banners2.jpg'
+import Banners3 from '../../../../public/Images/Home/banners3.jpg'
+import Banners4 from '../../../../public/Images/Home/banners4.jpg'
 import { ArrowIcon, DownArrow } from '../../../../public/SVG/IconsSvg'
 
 const Hero = () => {
     const [num, setnum] = useState(0)
     const Images = [
-        Banner5, Banner6, Banner7
+        Banners1, Banners2, Banners3,Banners4
     ]
     const show = Images[num]
 
@@ -25,14 +23,14 @@ const Hero = () => {
 
 
     return (
-        <div className=' selection:hidden p-3 flex  text-white h-[20rem]  justify-center  '>
-            <div className=' sm:w-[55rem] relative'>
-            <h1 onClick={() => num == 0 ? setnum(Images.length - 1) : setnum(num - 1)} className=' rotate-180 transform   absolute left-5  text-black z-50 top-[40%]  bg-gray-200 py-4 px-2 cursor-pointer rounded-md '>
-                <svg width="1rem" height="1rem" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><polygon points="150.46 478 129.86 456.5 339.11 256 129.86 55.49 150.46 34 382.14 256 150.46 478" /></svg>
+        <div className=' selection:hidden p-3 flex bg-slate-100  text-white h-[20rem]  justify-center  '>
+            <div className=' relative'>
+            <h1  onClick={() => num == 0 ? setnum(Images.length - 1) : setnum(num - 1)} className=' rotate-180 transform   absolute -left-10   z-50 top-[40%]   bg-gray-400 py-4 px-2 cursor-pointer rounded-md '>
+                <svg fill='white' width="1rem" height="1rem" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><polygon points="150.46 478 129.86 456.5 339.11 256 129.86 55.49 150.46 34 382.14 256 150.46 478" /></svg>
             </h1>
             <img src={show} alt="" className=' w-full h-full   ' />
-            <h1 onClick={() => Images.length - 1 != num ? setnum(num + 1) : setnum(0)} className=' absolute right-5 z-50  top-[40%] bg-gray-200 py-4 px-2 cursor-pointer rounded-md'>
-                <svg width="1rem" height="1rem" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><polygon points="150.46 478 129.86 456.5 339.11 256 129.86 55.49 150.46 34 382.14 256 150.46 478" /></svg>
+            <h1 onClick={() => Images.length - 1 != num ? setnum(num + 1) : setnum(0)} className=' absolute -right-10 z-50  top-[40%] bg-gray-400 py-4 px-2 cursor-pointer rounded-md'>
+                <svg  fill='white' width="1rem" height="1rem" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><polygon points="150.46 478 129.86 456.5 339.11 256 129.86 55.49 150.46 34 382.14 256 150.46 478" /></svg>
             </h1>
             </div>
         </div>
